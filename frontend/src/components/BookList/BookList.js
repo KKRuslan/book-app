94c0ba820e372e090e72e5973ae460f5a47f2700
KKRuslan <karpetskyu17@gmail.com>
2024-07-13 13:51:56 +0300

    Filter redux slice and filter by title

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,11 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { deleteBook, toggleFavorite } from '../../redux/books/actionCreators'
 import { BsBookmarkStar, BsBookmarkStarFill } from 'react-icons/bs'
-
+import { selectTitleFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 const BookList = () => {
   const books = useSelector((state) => state.books)
+  const titleFilter = useSelector(selectTitleFilter)
   const dispatch = useDispatch()
 
   const handleDeleteBook = (id) => {
@@ -16,6 +17,13 @@ const BookList = () => {
     dispatch(toggleFavorite(id))
   }
 
+  const filteredBooks = books.filter((book) => {
+    const matchesTitle = book.title
+      .toLowerCase()
+      .includes(titleFilter.toLowerCase())
+    return matchesTitle
+  })
+
   return (
     <div className="app-block book-list">
       <h2>BookList</h2>
@@ -23,7 +31,7 @@ const BookList = () => {
         <p>No books available</p>
       ) : (
         <ul>
-          {books.map((book, i) => (
+          {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from './books/reducer'
+import filterReducder from './slices/filterSlice'
 
 const store = configureStore({
   reducer: {
-    books: booksReducer
+    books: booksReducer,
+    filter: filterReducder,
   },
 })
 
